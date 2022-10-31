6d7fb4134fbaf4ba7988836bc3f636d1077df9ec
Carlo Magno Chuyes Romero <carlosgtmam@gmail.com>
2022-10-31 16:14:48 -0400

    Error con el context

--- WebApplication4/Models/Project_DesmodusDBContext.cs
+++ WebApplication4/Models/Project_DesmodusDBContext.cs
@@ -41,7 +41,7 @@ namespace WebApplication4.Models
             modelBuilder.Entity<TbAcceso>(entity =>
             {
                 entity.HasKey(e => e.IdAcceso)
-                    .HasName("PK__tb_Acces__99B2858FA626FFE9");
+                    .HasName("PK__tb_Acces__99B2858FA19F9C43");
 
                 entity.ToTable("tb_Acceso");
 
@@ -67,7 +67,7 @@ namespace WebApplication4.Models
             modelBuilder.Entity<TbCategorium>(entity =>
             {
                 entity.HasKey(e => e.IdProblema)
-                    .HasName("PK__tb_Categ__0FDB5F4A188DEFD0");
+                    .HasName("PK__tb_Categ__0FDB5F4AE4EF15D0");
 
                 entity.ToTable("tb_Categoria");
 
@@ -76,10 +76,28 @@ namespace WebApplication4.Models
                     .IsUnicode(false);
             });
 
+            modelBuilder.Entity<TbDerivado>(entity =>
+            {
+                entity.HasKey(e => e.IdDerivado)
+                    .HasName("PK__tb_Deriv__91186DFAB5B98C71");
+
+                entity.ToTable("tb_Derivado");
+
+                entity.HasOne(d => d.IdTicketNavigation)
+                    .WithMany(p => p.TbDerivados)
+                    .HasForeignKey(d => d.IdTicket)
+                    .HasConstraintName("FK__tb_Deriva__IdTic__5DCAEF64");
+
+                entity.HasOne(d => d.IdUsuarioNavigation)
+                    .WithMany(p => p.TbDerivados)
+                    .HasForeignKey(d => d.IdUsuario)
+                    .HasConstraintName("FK__tb_Deriva__IdUsu__5CD6CB2B");
+            });
+
             modelBuilder.Entity<TbEmpresa>(entity =>
             {
                 entity.HasKey(e => e.IdEmpresa)
-                    .HasName("PK__tb_Empre__5EF4033EA8619F96");
+                    .HasName("PK__tb_Empre__5EF4033EA856AECB");
 
                 entity.ToTable("tb_Empresa");
 
@@ -99,7 +117,7 @@ namespace WebApplication4.Models
             modelBuilder.Entity<TbEstadoTicket>(entity =>
             {
                 entity.HasKey(e => e.IdEstado)
-                    .HasName("PK__tb_Estad__FBB0EDC1ABF769E3");
+                    .HasName("PK__tb_Estad__FBB0EDC15FECE42D");
 
                 entity.ToTable("tb_EstadoTicket");
 
@@ -111,7 +129,7 @@ namespace WebApplication4.Models
             modelBuilder.Entity<TbFechaTicket>(entity =>
             {
                 entity.HasKey(e => e.IdFecha)
-                    .HasName("PK__tb_Fecha__8D0F205A4C8383F0");
+                    .HasName("PK__tb_Fecha__8D0F205AB76F2122");
 
                 entity.ToTable("tb_FechaTicket");
 
@@ -123,7 +141,7 @@ namespace WebApplication4.Models
             modelBuilder.Entity<TbPermiso>(entity =>
             {
                 entity.HasKey(e => e.IdPermiso)
-                    .HasName("PK__tb_Permi__0D626EC81DB16237");
+                    .HasName("PK__tb_Permi__0D626EC8CAADEBFD");
 
                 entity.ToTable("tb_Permiso");
 
@@ -135,7 +153,7 @@ namespace WebApplication4.Models
             modelBuilder.Entity<TbPrioridadTicket>(entity =>
             {
                 entity.HasKey(e => e.IdPrioridad)
-                    .HasName("PK__tb_Prior__0FC70DD509F60752");
+                    .HasName("PK__tb_Prior__0FC70DD5FC0D152B");
 
                 entity.ToTable("tb_PrioridadTicket");
 
@@ -147,7 +165,7 @@ namespace WebApplication4.Models
             modelBuilder.Entity<TbTicket>(entity =>
             {
                 entity.HasKey(e => e.IdTicket)
-                    .HasName("PK__tb_Ticke__4B93C7E72AC23AFD");
+                    .HasName("PK__tb_Ticke__4B93C7E78A71EAAA");
 
                 entity.ToTable("tb_Ticket");
 
@@ -166,33 +184,33 @@ namespace WebApplication4.Models
                 entity.HasOne(d => d.IdEstadoNavigation)
                     .WithMany(p => p.TbTickets)
                     .HasForeignKey(d => d.IdEstado)
-                    .HasConstraintName("FK__tb_Ticket__IdEst__5070F446");
+                    .HasConstraintName("FK__tb_Ticket__IdEst__534D60F1");
 
                 entity.HasOne(d => d.IdFechaNavigation)
                     .WithMany(p => p.TbTickets)
                     .HasForeignKey(d => d.IdFecha)
-                    .HasConstraintName("FK__tb_Ticket__IdFec__5165187F");
+                    .HasConstraintName("FK__tb_Ticket__IdFec__5441852A");
 
                 entity.HasOne(d => d.IdPrioridadNavigation)
                     .WithMany(p => p.TbTickets)
                     .HasForeignKey(d => d.IdPrioridad)
-                    .HasConstraintName("FK__tb_Ticket__IdPri__4F7CD00D");
+                    .HasConstraintName("FK__tb_Ticket__IdPri__52593CB8");
 
                 entity.HasOne(d => d.IdProblemaNavigation)
                     .WithMany(p => p.TbTickets)
                     .HasForeignKey(d => d.IdProblema)
-                    .HasConstraintName("FK__tb_Ticket__IdPro__52593CB8");
+                    .HasConstraintName("FK__tb_Ticket__IdPro__5535A963");
 
                 entity.HasOne(d => d.IdUsuarioNavigation)
                     .WithMany(p => p.TbTickets)
                     .HasForeignKey(d => d.IdUsuario)
-                    .HasConstraintName("FK__tb_Ticket__IdUsu__4E88ABD4");
+                    .HasConstraintName("FK__tb_Ticket__IdUsu__5165187F");
             });
 
             modelBuilder.Entity<TbUsuario>(entity =>
             {
                 entity.HasKey(e => e.IdUsuario)
-                    .HasName("PK__tb_Usuar__5B65BF973DC080F2");
+                    .HasName("PK__tb_Usuar__5B65BF97D9C40BDF");
 
                 entity.ToTable("tb_Usuario");
 
