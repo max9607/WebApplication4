9ce43a910ce6b19a35d5552ab61dae6117a4d0ac
Aldunate <sc.anthony.aldunate.j@upds.net.b>
2022-11-13 10:06:09 -0400

    añadicion de tabla de cerracion de tickets

---
+++ WebApplication4/Controllers/TbTicketsCerradoesController.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using WebApplication4.Models;
+
+namespace WebApplication4.Controllers
+{
+    public class TbTicketsCerradoesController : Controller
+    {
+        private readonly Project_DesmodusDBContext _context;
+
+        public TbTicketsCerradoesController(Project_DesmodusDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: TbTicketsCerradoes
+        public async Task<IActionResult> Index()
+        {
+              return View(await _context.TbTicketsCerrados.ToListAsync());
+        }
+
+        // GET: TbTicketsCerradoes/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.TbTicketsCerrados == null)
+            {
+                return NotFound();
+            }
+
+            var tbTicketsCerrado = await _context.TbTicketsCerrados
+                .FirstOrDefaultAsync(m => m.IdCerrados == id);
+            if (tbTicketsCerrado == null)
+            {
+                return NotFound();
+            }
+
+            return View(tbTicketsCerrado);
+        }
+
+        // GET: TbTicketsCerradoes/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: TbTicketsCerradoes/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("IdCerrados,IdTicket,Cliente,Comentario,DespricionP,FechaCreado,FechaCerrado,Receptor")] TbTicketsCerrado tbTicketsCerrado)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(tbTicketsCerrado);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(tbTicketsCerrado);
+        }
+
+        // GET: TbTicketsCerradoes/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.TbTicketsCerrados == null)
+            {
+                return NotFound();
+            }
+
+            var tbTicketsCerrado = await _context.TbTicketsCerrados.FindAsync(id);
+            if (tbTicketsCerrado == null)
+            {
+                return NotFound();
+            }
+            return View(tbTicketsCerrado);
+        }
+
+        // POST: TbTicketsCerradoes/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("IdCerrados,IdTicket,Cliente,Comentario,DespricionP,FechaCreado,FechaCerrado,Receptor")] TbTicketsCerrado tbTicketsCerrado)
+        {
+            if (id != tbTicketsCerrado.IdCerrados)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(tbTicketsCerrado);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TbTicketsCerradoExists(tbTicketsCerrado.IdCerrados))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(tbTicketsCerrado);
+        }
+
+        // GET: TbTicketsCerradoes/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.TbTicketsCerrados == null)
+            {
+                return NotFound();
+            }
+
+            var tbTicketsCerrado = await _context.TbTicketsCerrados
+                .FirstOrDefaultAsync(m => m.IdCerrados == id);
+            if (tbTicketsCerrado == null)
+            {
+                return NotFound();
+            }
+
+            return View(tbTicketsCerrado);
+        }
+
+        // POST: TbTicketsCerradoes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.TbTicketsCerrados == null)
+            {
+                return Problem("Entity set 'Project_DesmodusDBContext.TbTicketsCerrados'  is null.");
+            }
+            var tbTicketsCerrado = await _context.TbTicketsCerrados.FindAsync(id);
+            if (tbTicketsCerrado != null)
+            {
+                _context.TbTicketsCerrados.Remove(tbTicketsCerrado);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool TbTicketsCerradoExists(int id)
+        {
+          return _context.TbTicketsCerrados.Any(e => e.IdCerrados == id);
+        }
+    }
+}
--- WebApplication4/Models/Project_DesmodusDBContext.cs
+++ WebApplication4/Models/Project_DesmodusDBContext.cs
@@ -25,6 +25,7 @@ namespace WebApplication4.Models
         public virtual DbSet<TbPermiso> TbPermisos { get; set; } = null!;
         public virtual DbSet<TbPrioridadTicket> TbPrioridadTickets { get; set; } = null!;
         public virtual DbSet<TbTicket> TbTickets { get; set; } = null!;
+        public virtual DbSet<TbTicketsCerrado> TbTicketsCerrados { get; set; } = null!;
         public virtual DbSet<TbUsuario> TbUsuarios { get; set; } = null!;
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
@@ -40,7 +41,7 @@ namespace WebApplication4.Models
             modelBuilder.Entity<TbAcceso>(entity =>
             {
                 entity.HasKey(e => e.IdAcceso)
-                    .HasName("PK__tb_Acces__99B2858FA19F9C43");
+                    .HasName("PK__tb_Acces__99B2858FA626FFE9");
 
                 entity.ToTable("tb_Acceso");
 
@@ -66,7 +67,7 @@ namespace WebApplication4.Models
             modelBuilder.Entity<TbCategorium>(entity =>
             {
                 entity.HasKey(e => e.IdProblema)
-                    .HasName("PK__tb_Categ__0FDB5F4AE4EF15D0");
+                    .HasName("PK__tb_Categ__0FDB5F4AA41314AA");
 
                 entity.ToTable("tb_Categoria");
 
@@ -78,25 +79,25 @@ namespace WebApplication4.Models
             modelBuilder.Entity<TbDerivado>(entity =>
             {
                 entity.HasKey(e => e.IdDerivado)
-                    .HasName("PK__tb_Deriv__91186DFAB5B98C71");
+                    .HasName("PK__tb_Deriv__91186DFA631C9159");
 
                 entity.ToTable("tb_Derivado");
 
                 entity.HasOne(d => d.IdTicketNavigation)
                     .WithMany(p => p.TbDerivados)
                     .HasForeignKey(d => d.IdTicket)
-                    .HasConstraintName("FK__tb_Deriva__IdTic__5DCAEF64");
+                    .HasConstraintName("FK__tb_Deriva__IdTic__4AB81AF0");
 
                 entity.HasOne(d => d.IdUsuarioNavigation)
                     .WithMany(p => p.TbDerivados)
                     .HasForeignKey(d => d.IdUsuario)
-                    .HasConstraintName("FK__tb_Deriva__IdUsu__5CD6CB2B");
+                    .HasConstraintName("FK__tb_Deriva__IdUsu__49C3F6B7");
             });
 
             modelBuilder.Entity<TbEmpresa>(entity =>
             {
                 entity.HasKey(e => e.IdEmpresa)
-                    .HasName("PK__tb_Empre__5EF4033EA856AECB");
+                    .HasName("PK__tb_Empre__5EF4033E096EDF8A");
 
                 entity.ToTable("tb_Empresa");
 
@@ -116,7 +117,7 @@ namespace WebApplication4.Models
             modelBuilder.Entity<TbEstadoTicket>(entity =>
             {
                 entity.HasKey(e => e.IdEstado)
-                    .HasName("PK__tb_Estad__FBB0EDC15FECE42D");
+                    .HasName("PK__tb_Estad__FBB0EDC1D2963AAB");
 
                 entity.ToTable("tb_EstadoTicket");
 
@@ -128,7 +129,7 @@ namespace WebApplication4.Models
             modelBuilder.Entity<TbFechaTicket>(entity =>
             {
                 entity.HasKey(e => e.IdFecha)
-                    .HasName("PK__tb_Fecha__8D0F205AB76F2122");
+                    .HasName("PK__tb_Fecha__8D0F205AC58090FF");
 
                 entity.ToTable("tb_FechaTicket");
 
@@ -140,7 +141,7 @@ namespace WebApplication4.Models
             modelBuilder.Entity<TbPermiso>(entity =>
             {
                 entity.HasKey(e => e.IdPermiso)
-                    .HasName("PK__tb_Permi__0D626EC8CAADEBFD");
+                    .HasName("PK__tb_Permi__0D626EC8659BB50A");
 
                 entity.ToTable("tb_Permiso");
 
@@ -152,7 +153,7 @@ namespace WebApplication4.Models
             modelBuilder.Entity<TbPrioridadTicket>(entity =>
             {
                 entity.HasKey(e => e.IdPrioridad)
-                    .HasName("PK__tb_Prior__0FC70DD5FC0D152B");
+                    .HasName("PK__tb_Prior__0FC70DD5A383A030");
 
                 entity.ToTable("tb_PrioridadTicket");
 
@@ -164,7 +165,7 @@ namespace WebApplication4.Models
             modelBuilder.Entity<TbTicket>(entity =>
             {
                 entity.HasKey(e => e.IdTicket)
-                    .HasName("PK__tb_Ticke__4B93C7E78A71EAAA");
+                    .HasName("PK__tb_Ticke__4B93C7E79D9D8719");
 
                 entity.ToTable("tb_Ticket");
 
@@ -183,33 +184,61 @@ namespace WebApplication4.Models
                 entity.HasOne(d => d.IdEstadoNavigation)
                     .WithMany(p => p.TbTickets)
                     .HasForeignKey(d => d.IdEstado)
-                    .HasConstraintName("FK__tb_Ticket__IdEst__534D60F1");
+                    .HasConstraintName("FK__tb_Ticket__IdEst__38996AB5");
 
                 entity.HasOne(d => d.IdFechaNavigation)
                     .WithMany(p => p.TbTickets)
                     .HasForeignKey(d => d.IdFecha)
-                    .HasConstraintName("FK__tb_Ticket__IdFec__5441852A");
+                    .HasConstraintName("FK__tb_Ticket__IdFec__398D8EEE");
 
                 entity.HasOne(d => d.IdPrioridadNavigation)
                     .WithMany(p => p.TbTickets)
                     .HasForeignKey(d => d.IdPrioridad)
-                    .HasConstraintName("FK__tb_Ticket__IdPri__52593CB8");
+                    .HasConstraintName("FK__tb_Ticket__IdPri__37A5467C");
 
                 entity.HasOne(d => d.IdProblemaNavigation)
                     .WithMany(p => p.TbTickets)
                     .HasForeignKey(d => d.IdProblema)
-                    .HasConstraintName("FK__tb_Ticket__IdPro__5535A963");
+                    .HasConstraintName("FK__tb_Ticket__IdPro__3A81B327");
 
                 entity.HasOne(d => d.IdUsuarioNavigation)
                     .WithMany(p => p.TbTickets)
                     .HasForeignKey(d => d.IdUsuario)
-                    .HasConstraintName("FK__tb_Ticket__IdUsu__5165187F");
+                    .HasConstraintName("FK__tb_Ticket__IdUsu__36B12243");
+            });
+
+            modelBuilder.Entity<TbTicketsCerrado>(entity =>
+            {
+                entity.HasKey(e => e.IdCerrados)
+                    .HasName("PK__tb_Ticke__6EA883BB786761BA");
+
+                entity.ToTable("tb_TicketsCerrados");
+
+                entity.Property(e => e.Cliente)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Comentario)
+                    .HasMaxLength(200)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.DespricionP)
+                    .HasMaxLength(100)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.FechaCerrado).HasColumnType("datetime");
+
+                entity.Property(e => e.FechaCreado).HasColumnType("datetime");
+
+                entity.Property(e => e.Receptor)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
             });
 
             modelBuilder.Entity<TbUsuario>(entity =>
             {
                 entity.HasKey(e => e.IdUsuario)
-                    .HasName("PK__tb_Usuar__5B65BF97D9C40BDF");
+                    .HasName("PK__tb_Usuar__5B65BF97907B1255");
 
                 entity.ToTable("tb_Usuario");
 
