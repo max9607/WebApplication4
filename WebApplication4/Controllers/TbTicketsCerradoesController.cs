9ee0269f7b51f6a295be7a879a3742ac8281b7f2
Carlo Magno Chuyes Romero <carlosgtmam@gmail.com>
2022-11-15 19:34:47 -0400

    Cambios Minimos

--- WebApplication4/Controllers/TbTicketsCerradoesController.cs
+++ WebApplication4/Controllers/TbTicketsCerradoesController.cs
@@ -23,12 +23,12 @@ namespace WebApplication4.Controllers
         public async Task<IActionResult> Index(DateTime? FechaInicio, DateTime? FechaFinal)
         {
             var date = from m in _context.TbTicketsCerrados select m;
-            Console.WriteLine("FechaInicio "+FechaInicio.ToString());
-            Console.WriteLine("FechaFinal  "+FechaFinal.ToString());
+            //Console.WriteLine("FechaInicio "+FechaInicio.ToString());
+            //Console.WriteLine("FechaFinal  "+FechaFinal.ToString());
             
             if (FechaInicio != null && FechaFinal != null)
             {
-                date = date.Where(e => e.FechaCreado >= FechaInicio || e.FechaCerrado <= FechaFinal);
+                date = date.Where(e => e.FechaCreado >= FechaInicio && e.FechaCreado <= FechaFinal.Value.AddDays(1));
  
                 return View(await date.ToListAsync());
             }
