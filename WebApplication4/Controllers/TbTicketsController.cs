67c79cb322c835758f6b6ec1517e137ad4c8ecc0
Carlo Magno Chuyes Romero <carlosgtmam@gmail.com>
2022-11-26 13:29:16 -0400

    Desactivada opción de enviar correo al crear ticket

--- WebApplication4/Controllers/TbTicketsController.cs
+++ WebApplication4/Controllers/TbTicketsController.cs
@@ -174,7 +174,7 @@ namespace WebApplication4.Controllers
 
 
                 }
-                mailLogica omail = new mailLogica();
+                /*mailLogica omail = new mailLogica();
 
                 var admins = _context.TbAccesos.Where(i => i.IdPermisoNavigation.Nombre == "Administrador").ToList();
                 var idadmins = admins.Select(i => i.IdUsuario).ToList();
@@ -184,7 +184,7 @@ namespace WebApplication4.Controllers
                 var correoUsuario = _context.TbUsuarios.First(i => i.IdUsuario == tbTicket.IdUsuario);
                 
 
-                await omail.SendEmailAsync(listacorreos,tbTicket.DespricionP,correoUsuario.Correo);
+                await omail.SendEmailAsync(listacorreos,tbTicket.DespricionP,correoUsuario.Correo);*/
 
                 _context.Add(tbTicket);
                 await _context.SaveChangesAsync();
