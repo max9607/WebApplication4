90c733886f30504c6a2132c2de0baf651a807ebc
anthony gabriel aldunate justiniano <sc.anthony.aldunate.j@upds.net.bo>
2023-10-08 16:53:02 -0400

    implementada solucion para evitar el roll bak de usuarios y vista de usuarios en creacion de tickets corregida

---
+++ WebApplication4/Views/Shared/_SideNav.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication4.Views.Shared
+{
+    public class _SideNavModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
